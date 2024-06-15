24f558930df94b5d7eece6198c5753957a75aa31
Hirushiharan Thevendran <hirushiharant@gmail.com>
2024-06-15 11:52:32 +0530

    feat: configure jar

--- scripts/python/data_collection.py
+++ scripts/python/data_collection.py
@@ -15,12 +15,12 @@ MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))  # Default to 3306 if not set
 # Initialize SparkSession
 spark = SparkSession.builder \
     .appName("Telecom DB MySQL View to HDFS") \
-    .config("spark.jars", "/usr/local/spark/jars/mysql-connector-j-8.0.33.tar.gz") \
+    .config("spark.jars", "/usr/local/spark/jars/mysql-connector-j-8.0.33.jar") \
     .getOrCreate()
 
 # Define MySQL connection properties
 mysql_props = {
-    "url": "jdbc:mysql://" + MYSQL_HOST + ":"+ MYSQL_PORT + "/" + MYSQL_DATABASE,
+    "url": f"jdbc:mysql://{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}",
     "driver": "com.mysql.jdbc.Driver",
     "dbtable": "(SELECT * FROM call_details_view) AS call_details_view",
     "user": MYSQL_USER,
