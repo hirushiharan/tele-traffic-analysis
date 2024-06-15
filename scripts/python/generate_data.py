7e56c656f7a208a77389b8bc2caffe191f38130f
Hirushiharan Thevendran <hirushiharant@gmail.com>
2024-06-15 08:57:41 +0530

    refact: format code

--- scripts/python/generate_data.py
+++ scripts/python/generate_data.py
@@ -30,7 +30,7 @@ load_dotenv()
 # MySQL database credentials
 MYSQL_ROOT_PASSWORD = os.getenv("MYSQL_ROOT_PASSWORD")
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
-MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_USER = os.getenv("MYSQL_USER", "root") # Default to root if not set
 MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost") # Default to localhost if not set
 MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))  # Default to 3306 if not set
 
